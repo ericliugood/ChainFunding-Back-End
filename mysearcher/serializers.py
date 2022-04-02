1c87540d54f22ff8b3629fa0842d141e1cf0a0d2
curtis <curtisyan90@gmail.com>
2022-04-02 22:37:47 +0800

    Create serializers

---
+++ mysearcher/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from mydatabase.models import FundingProjects
+
+
+class SearcherSerializer(ModelSerializer):
+    class Meta:
+        model = FundingProjects
+        fields = '__all__'
