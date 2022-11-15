95ea1e6cf15a8adbd2ce376e2409f3385217653d
curtis <curtisyan90@gmail.com>
2022-11-16 03:26:18 +0800

    Complete searcher let funding can searched by raiser, NFT's name, NFT's address

--- mysearcher/views.py
+++ mysearcher/views.py
@@ -1,6 +1,8 @@
 from mysearcher.serializers import SearcherSerializer
 from mydatabase.models import FundingProjects, UserDatas
 
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 
@@ -15,14 +17,21 @@ class SearcherViewSet(ModelViewSet):
     @action(detail=False)
     def project(self, request):  # project nft user
         data = request.query_params
+        parm = data.get('parm')
 
-        nftid = data.get('nftid')
-        fundraiser = data.get('fundraiser')
-
-        if fundraiser is not None:  # find project from raiser
+        if parm is not None:  # find project from raiser
             try:
-                fundraiser = UserDatas.objects.get(usernameAccount=fundraiser)  # find fundraiser
-                raiser_project = FundingProjects.objects.filter(fundraiser_id=fundraiser.id)  # find project by raiser
+                try:
+                    fundraiser = UserDatas.objects.get(usernameAccount__contains=parm)  # find fundraiser
+
+                    raiser_project = FundingProjects.objects.filter(
+                        Q(fundraiser_id=fundraiser.id) | Q(nftName__contains=parm) | Q(nftContractAddress__contains=parm)
+                    )  # find project by raiser, nftname, nftid, nftaddr
+                except ObjectDoesNotExist:
+                    raiser_project = FundingProjects.objects.filter(
+                        Q(nftName__contains=parm) | Q(nftContractAddress__contains=parm)
+                    )  # find project by nftname, nftid, nftaddr
+
                 return success({
                     'project': [{
                         'nftId': p.nftId,
@@ -34,7 +43,7 @@ class SearcherViewSet(ModelViewSet):
                         'butPrice': p.buyPrice,
                         'sellPrice': p.sellPrice,
                         'gasPrice': p.gasPrice,
-                        'fundraiser': fundraiser.usernameAccount
+                        'fundraiser': UserDatas.objects.filter(id=p.fundraiser_id).values('usernameAccount')
                     }
                         for p in raiser_project
                     ]
@@ -42,46 +51,48 @@ class SearcherViewSet(ModelViewSet):
             except Exception as e:
                 print(e)
                 return notfound(Msg.NotFound.project)
-        elif nftid is not None:  # find project from nftid
-            try:
-                nft_project = FundingProjects.objects.filter(nftId=nftid)  # find project by nft id
-                return success({
-                    'project': [{
-                        'nftid': p.nftId,
-                        'startTime': p.startTime,
-                        'endTime': p.endTime,
-                        'token': p.token,
-                        'butPrice': p.buyPrice,
-                        'sellPrice': p.sellPrice,
-                        'gasPrice': p.gasPrice
-                    }
-                        for p in nft_project
-                    ]
-                })
-            except Exception as e:
-                print(e)
-                return notfound(Msg.NotFound.project)
-        else:  # list all project
-            try:
-                project = FundingProjects.objects.all()
-                return success({
-                    'project': [{
-                        'nftid': p.nftId,
-                        'startTime': p.startTime,
-                        'endTime': p.endTime,
-                        'token': p.token,
-                        'butPrice': p.buyPrice,
-                        'sellPrice': p.sellPrice,
-                        'gasPrice': p.gasPrice
-                    }
-                        for p in project
-                    ]
-                })
-            except Exception as e:
-                print(e)
-                return notfound(Msg.NotFound.project)
+        else:
+            return err(Msg.Err.FundingProject.search)
+        # elif nftid is not None:  # find project from nftid
+        #     try:
+        #         nft_project = FundingProjects.objects.filter(nftId=nftid)  # find project by nft id
+        #         return success({
+        #             'project': [{
+        #                 'nftid': p.nftId,
+        #                 'startTime': p.startTime,
+        #                 'endTime': p.endTime,
+        #                 'token': p.token,
+        #                 'butPrice': p.buyPrice,
+        #                 'sellPrice': p.sellPrice,
+        #                 'gasPrice': p.gasPrice
+        #             }
+        #                 for p in nft_project
+        #             ]
+        #         })
+        #     except Exception as e:
+        #         print(e)
+        #         return notfound(Msg.NotFound.project)
+        # else:  # list all project
+        #     try:
+        #         project = FundingProjects.objects.all()
+        #         return success({
+        #             'project': [{
+        #                 'nftid': p.nftId,
+        #                 'startTime': p.startTime,
+        #                 'endTime': p.endTime,
+        #                 'token': p.token,
+        #                 'butPrice': p.buyPrice,
+        #                 'sellPrice': p.sellPrice,
+        #                 'gasPrice': p.gasPrice
+        #             }
+        #                 for p in project
+        #             ]
+        #         })
+        #     except Exception as e:
+        #         print(e)
+        #         return notfound(Msg.NotFound.project)
 
     @action(detail=False)
-    def nft(self, request):
+    def opensea(self, request):
         data = request.query_params
         nftid = data.get('nftid')
