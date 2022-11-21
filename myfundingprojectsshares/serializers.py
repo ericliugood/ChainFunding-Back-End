5cbb91e6322579da2fccd2a5fd9bc29b8965eb0e
LWL <ericliugood@gmail.com>
2022-11-22 01:33:37 +0800

    API得到集資進度
    API得到集資進度

--- myfundingprojectsshares/serializers.py
+++ myfundingprojectsshares/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from mydatabase.models import  FundingShares as Share 
+from mydatabase.models import  FundingShares as Share ,SharesSold
 
 class FundingSharesSerializer(ModelSerializer):
     class Meta:
         model = Share
         fields = ['id','fundingProject', 'share']
+
+class FundingSharesSoldSerializer(ModelSerializer):
+    class Meta:
+        model = SharesSold
+        fields = ['id','fundingProject', 'share','price','token']
