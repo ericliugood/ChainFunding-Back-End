bb78a2386712718670ae921b51f899a0c16a2419
LWL <ericliugood@gmail.com>
2022-12-04 14:18:05 +0800

    新增特殊權限
    新增特殊權限

--- myfundingprojects/views2.py
+++ myfundingprojects/views2.py
@@ -53,10 +53,10 @@ class FundingProjectsViewSet2(viewsets.ModelViewSet):
         elif self.action == 'update':
             permission_classes = [IsAuthenticated]
             return [permission() for permission in permission_classes]
-        elif self.action == 'like':
+        elif self.action == 'destroy':
             permission_classes = [IsAuthenticated]
             return [permission() for permission in permission_classes]
-        elif self.action == 'retrieve':
+        elif self.action == 'like':
             permission_classes = [IsAuthenticated]
             return [permission() for permission in permission_classes]
         return[]
